35d8a6a2e4955c6cf09efd1d9e6b62690a494f7c
Khanh Hua <giakhanh2487@gmail.com>
2017-02-13 17:13:38 +0000

    API testing

--- test/models/test_product.py
+++ test/models/test_product.py
@@ -1,13 +1,13 @@
 from app.models import Product
 
-def test_product_instantiation():
+def test_instantiation():
     product = Product(id="ID001", name="Test Product", kind="generic")
     
     assert product.id == "ID001"
     assert product.name == "Test Product"
     assert product.kind == "generic"
     
-def test_product_serialization():
+def test_serialization():
     product = Product(id="ID001", name="Test Product", kind="generic")
     
     dict_data = product.to_dict()
